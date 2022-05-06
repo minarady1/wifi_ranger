d5985df4f2e48e753bb226fc270e8192c00828ea
minarady1 <minarady@gmail.com>
2022-05-06 15:41:22 +0200

    added duration limit for experiment added network info + experiment info on pakcet payload

--- client.py
+++ client.py
@@ -15,13 +15,14 @@ import json
 from sys import getsizeof
 import pdb
 import sys
+import os
 
 
 CONN=  "TCP"
-PACKET_SIZE = 500   # B 
+PACKET_SIZE = 1000000   # B 
 FREQ        = 0.1   # s
 seqnum      = 0
-
+DURATION     = 5 # s
 USAGE = "Usage: client.py <period (s)> <connection type(TCP/UDP)> <packet size (B)>"
 
 def preparePacket():
@@ -35,11 +36,14 @@ def preparePacket():
 
     seqnum_b = seqnum.to_bytes(80,'big')
 
-    # get rssi
+    # get networkinfo
 
-    # todo
+    network_info_b = get_network_info()
+    
+    # Experiment info
+    exp_info_b = get_exp_info()
 
-    header = timestamp_b + seqnum_b
+    header = timestamp_b + seqnum_b + exp_info_b + network_info_b 
 
     # add additional payload
 
@@ -48,7 +52,7 @@ def preparePacket():
     print ("#", timestamp,seqnum ,getsizeof (header) )
     
     # append all in one bye array
-    payload = header+ padding 
+    payload = header+ padding
     
     seqnum = seqnum+1
     return payload
@@ -62,10 +66,35 @@ def send_udp(payload):
 def send_tcp(payload):
     s.sendall(payload)
 
+def get_network_info():
+    stream = os.popen("netsh wlan show interfaces")
+    output = stream.read()
+    output_b = bytes(output,"utf-8")
+    print (output)
+    len_net_info = getsizeof(output_b)
+    if (len_net_info>5000):
+        # problem
+        pdb.set_trace()
+    res = bytearray(5000) # max netinfo size 5kb
+    i = 0
+    for item in output_b:
+        res [i] = item
+        i=i+1
+    return res
+
+def get_exp_info ():
+    packet_size = PACKET_SIZE.to_bytes(10,'big')
+    freq        = FREQ.to_bytes(10,'big') 
+    conn        = CONN.encode("utf-8") # 3 B
+    res = packet_size + freq+ conn # total 23 B
+    return res
+     
+
+
 print (USAGE)
 
 if (len(sys.argv)>3):
-    FREQ        = float(sys.argv[1])
+    FREQ        = int(sys.argv[1])
     CONN        = sys.argv[2]
     PACKET_SIZE = int(sys.argv[3])
 else:
@@ -75,18 +104,21 @@ else:
 starttime = time.time()
 
 if (CONN == "UDP"):
-    UDP_IP      = "10.90.90.1"
+    UDP_IP      = "10.90.90.2"
     UDP_PORT    = 5005
     print ("UDP target IP: %s" % UDP_IP)
     print ("UDP target port: %s" % UDP_PORT)
-    
-    while True:
+    period = 1/FREQ
+    start = time.time()
+    now = start
+    while (now-start< DURATION):
         payload = preparePacket()
         send_udp(payload)
-        time.sleep(FREQ - ((time.time() - starttime) % FREQ))
+        time.sleep(period - ((time.time() - starttime) % period))
+        now = time.time()
 
 if (CONN == "TCP"):
-    HOST = 'localhost'
+    HOST = '10.90.90.2'
     PORT = 50007
     print ("TCP Host: %s" % HOST)
     print ("TCP port: %s" % PORT)
@@ -115,15 +147,3 @@ if (CONN == "TCP"):
             payload = preparePacket()
             send_tcp(payload)
             time.sleep(1 - ((time.time() - starttime) % 1))
-
-
-# interface = '3b512542-7170-4cbd-ba73-285f37aa1c1f'
-# rssi_scanner = rssi.RSSI_Scan(interface)
-
-# ssids = ['dd-wrt','linksys']
-
-# # sudo argument automatixally gets set for 'false', if the 'true' is not set manually.
-# # python file will have to be run with sudo privileges.
-# ap_info = rssi_scanner.getAPinfo()
-
-# print(ap_info)
