119a2eea08b84e93619970640e1f357967a4d03c
minarady1 <minarady@gmail.com>
2022-07-11 20:13:52 +0200

    udp rx/rx code in python

---
+++ udp_rx.py
@@ -0,0 +1,118 @@
+'''
+Simple UDP/TCP server appplication with logging feautre.
+
+Bugwright2 Prject
+
+Mina Rady mina.rady@inria.fr
+
+'''
+
+import socket
+import sys
+import time
+import datetime
+import json
+import os
+import pdb
+from sys import getsizeof
+import sys
+
+
+
+USAGE = "Usage: udp_rx.py <host IP> <port> <duration (s)> <exp ID> <run ID>"
+print (USAGE)
+
+# log settings
+
+
+LOG_DIR_NAME = 'logs'
+UDP_IP      = sys.argv[1]
+UDP_PORT    = int(sys.argv[2])
+DURATION    = int(sys.argv[3])
+EXPID       = sys.argv[4]
+RUNID       = sys.argv[5]
+log_file_path = ''
+def prepare_log_file():
+    log_dir_path = os.path.join(os.path.dirname(__file__), LOG_DIR_NAME,RUNID)
+
+    # make sure we have the log directory
+    if os.path.isdir(log_dir_path):
+        # log directory is ready :-)
+        pass
+    else:
+        try:
+            os.mkdir(log_dir_path)
+        except OSError as err:
+            sys.exit('Failed to make the log directory: {}'.format(err))
+
+    # time.strftime('%Y%m%d-%H%M%S')
+    # decide a log file name and create it
+    
+    log_file_name = 'log_{}_{}-{}.jsonl'.format(EXPID, RUNID, time.strftime('%Y%m%d-%H%M%S'))
+    log_file_path = os.path.join(log_dir_path, log_file_name)
+    
+    if os.path.exists(log_file_path):
+        msg = (
+            'Failed to crate a log file.\n' +
+            'Log file already exits: {}'.format(log_file_path)
+        )
+        sys.exit(msg)
+    else:
+        # create an empty file with the log file name
+        try:
+            open(log_file_path, 'w').close()
+        except OSError as err:
+            sys.exit('Failed to create a log file: {}'.format(err))
+
+    return log_file_path
+
+def process_payload(data):
+    timestamp_ns    = int.from_bytes(data [0:80], "big")
+    seqnum          = int.from_bytes(data [80:160] , "big")
+    packet_size	    = int.from_bytes(data [160:170] , "big")
+    freq	        = int.from_bytes(data [170:180] , "big")
+  
+    
+    frame_len       = getsizeof(data) 
+    print(seqnum,frame_len)
+    res = {
+        "src_timestamp_ns"  : timestamp_ns,
+        "seqnum"            : seqnum,
+        "packet_size"       : packet_size,
+        "freq"	            : freq,
+        "frame_len"         : frame_len,
+        
+    }
+    return res
+
+def log_data (data):
+    global log_file_path
+    ts = datetime.datetime.now()
+    #pdb.set_trace()
+    payload_js = process_payload(data)
+    with open(log_file_path, 'a') as f:
+        log = {
+            'datetime': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+            'timestamp': time.time_ns(),
+            'payload'  : payload_js
+        }
+        f.write('{}\n'.format(json.dumps(log)))
+ 
+print (USAGE)
+
+
+log_file_path = prepare_log_file() 
+
+print (log_file_path)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+start = time.time()
+now = start
+while (now-start < DURATION):
+    data, addr = sock.recvfrom(65500) 
+    log_data(data)
+    now = time.time()
+print ("done, exiting  ...")
+sys.exit()
---
+++ udp_tx.py
@@ -0,0 +1,87 @@
+'''
+Simple UDP/TCP client appplication for Wi-Fi ranging.
+
+
+Bugwright2 Prject
+
+Mina Rady mina.rady@inria.fr
+
+'''
+
+import socket
+import rssi
+import time
+import json
+from sys import getsizeof
+import pdb
+import sys
+import os
+import logging
+
+
+
+PACKET_SIZE = 1000000   # B 
+FREQ        = 0.1   # s
+seqnum      = 0
+USAGE = "Usage: client.py <target IP> <port> <packet rate> <packet size (B)>"
+
+def preparePacket():
+    # get time
+    global seqnum
+    
+    timestamp  = time.time_ns()
+    timestamp_b = timestamp.to_bytes(80,'big')
+
+    # get seqnum
+
+    seqnum_b = seqnum.to_bytes(80,'big')
+
+    header = timestamp_b + seqnum_b 
+
+    # add additional payload
+
+    padding = bytearray([1]*(PACKET_SIZE- getsizeof (header)))
+    
+    
+    # append all in one bye array
+    payload = header+ padding
+    
+    seqnum = seqnum+1
+    return payload
+
+
+def send_udp(payload):
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.sendto(payload, (UDP_IP, UDP_PORT))
+
+
+
+
+print (USAGE)
+
+if (len(sys.argv)>3):
+    UDP_IP      = sys.argv[1]
+    UDP_PORT    = int(sys.argv[2])
+    FREQ        = int(sys.argv[3])
+    PACKET_SIZE = int(sys.argv[4])
+else:
+    exit()
+
+starttime = time.time()
+
+
+UDP_IP      = "localhost"
+UDP_PORT    = 6000
+print ("UDP target IP: %s" % UDP_IP)
+print ("UDP target port: %s" % UDP_PORT)
+period = 1/FREQ
+start = time.time()
+now = start
+while (True):
+    payload = preparePacket()
+    send_udp(payload)
+    time.sleep(period - ((time.time() - starttime) % period))
+    now = time.time()
+    print ("# sent: ", now,seqnum)
+
