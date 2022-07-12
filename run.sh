963e3017f59ea083253f2f72b355a75c4f188d05
minarady1 <minarady@gmail.com>
2022-07-12 15:52:41 +0200

    script uses ping for delay calculation

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 set -x
 echo "starting iperf with UDP stream.."
-python iperf.py $1 $2_udpstream "iperf3 -c localhost -p 4000 -t 15 -J -u -l 65500 -b 27M" &
-python iperf.py $1 $2_control_withudp "iperf3 -c localhost -p 3000 -t 15 -u -l 100 -J -R"
+python iperf.py $1 $2_tcpstream "iperf3 -c 10.90.90.1 -p 4000 -t 15 -J -b 27M" &
+# python udp_rx.py $1 $2_control_withtcp 10.90.90.2 6000
+ping 10.90.90.2 -n 15 -w 2000 >> logs/ping_$1_$2_tcpsteram.json
 echo "finished.."
 
 echo "starting iperf with UDP stream.."
-python iperf.py $1 $2_tcpstream "iperf3 -c localhost -p 4000 -t 15 -J -b 27M" &
-python iperf.py $1 $2_control_withtcp "iperf3 -c localhost -p 3000 -t 15 -u -l 100 -J -R"
+python iperf.py $1 $2_udpstream "iperf3 -c 10.90.90.1 -p 5000 -t 15 -J -u -l 65500 -b 27M" &
+#python udp_rx.py $1 $2_control_withudp 10.90.90.2 6000 15
+ping 10.90.90.2 -n 15 -w 2000 >> logs/ping_$1_$2_udpstream.json
 echo "finished.."
+
+
 $SHELL
