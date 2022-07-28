3baf109a3fde3ee851601f06c3431841e357b266
minarady1 <minarady@gmail.com>
2022-07-28 16:03:13 +0200

    wifi info tool for python that allows passive scanning of WiFi networks using the NetInfoView Tool, even if the net adapter is not in monitor mode and it stores the results, periodically in json format

---
+++ wifiinfo.py
@@ -0,0 +1,36 @@
+'''
+Script to do passive scanning of WiFi interface using NetInfoView tool
+It runs the scan for a priod of time and stores it into json format
+
+
+Bugwright2 Prject
+
+Mina Rady mina.rady@insa-lyon.fr
+'''
+
+import logging
+import os
+import io
+import time
+import subprocess
+import sys
+from subprocess import check_output
+
+
+expID = sys.argv[1]
+runID = sys.argv[2]
+DURATION     = int(sys.argv[3])
+period = 1
+
+start = time.time()
+counter = 1
+now = start
+while (now-start < DURATION):
+    print ((now-start )/10**6)
+    log_file_name = "{}_{}_{}_wifi.json".format(expID,runID, counter)
+    counter =counter +1
+    out = subprocess.run( ["WifiInfoView", "/DisplayMode", "1" ,"/sjson", log_file_name], shell=True, check=True, capture_output=True)
+    now = time.time()
+ 
+print ("done, exiting  ...")
+sys.exit()
